cb62e73913131940d0683339143f0b4d247b15df
Dmytro <74217403+DmytroCrawford@users.noreply.github.com>
2024-06-23 00:26:31 -0700

    Tailwing + starting

--- client/simple-english/popup.tsx
+++ client/simple-english/popup.tsx
@@ -1,25 +1,50 @@
-import { useState } from "react"
+import { AnimatePresence, motion } from "framer-motion"
+import { useReducer, useState } from "react"
+
+// import logo from "./assets/SimpleEnglish.svg"
+
+import "./style.css"
 
 function IndexPopup() {
   const [data, setData] = useState("")
+  const [count, increase] = useReducer((c) => c + 1, 0)
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <AnimatePresence>
+      (
+      <motion.div
+        exit={{
+          y: -20,
+          opacity: 0,
+          filter: "blur(5px)",
+          transition: { ease: "easeIn", duration: 0.22 }
+        }}
+        initial={{ opacity: 0, y: -15 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          filter: "blur(0px)",
+          transition: { type: "spring", duration: 0.7 }
+        }}
+        className="card">
+        <div
+          style={{
+            padding: 16
+          }}>
+          <h2 className="text-gray-900 font-bold text-xl mb-2">
+            Let's break it down
+          </h2>
+          <input onChange={(e) => setData(e.target.value)} value={data} />
+          <button
+            onClick={() => increase()}
+            type="button"
+            className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Simplify Page
+          </button>
+        </div>
+      </motion.div>
+      )
+    </AnimatePresence>
   )
 }
 
