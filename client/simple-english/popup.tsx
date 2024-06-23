9017a879b5f48a9d5ef8b1f29275b55258d5756e
Dmytro <74217403+DmytroCrawford@users.noreply.github.com>
2024-06-23 01:23:12 -0700

    Still cooking up, styled some CSS stuff. Going to go in tmmr.

--- client/simple-english/popup.tsx
+++ client/simple-english/popup.tsx
@@ -1,9 +1,42 @@
 import { AnimatePresence, motion } from "framer-motion"
 import { useReducer, useState } from "react"
 
-// import logo from "./assets/SimpleEnglish.svg"
-
 import "./style.css"
+
+let tabs = [
+  { id: "default", label: "Default" },
+  { id: "talktohelp", label: "Talk2Help" }
+]
+
+function AnimatedTabs() {
+  let [activeTab, setActiveTab] = useState(tabs[0].id)
+
+  return (
+    <div className="flex space-x-1">
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          onClick={() => setActiveTab(tab.id)}
+          className={`${
+            activeTab === tab.id ? "" : "hover:text-black/60"
+          } relative rounded-full px-3 py-1.5 text-sm font-medium text-white outline-sky-400 transition focus-visible:outline-2`}
+          style={{
+            WebkitTapHighlightColor: "transparent"
+          }}>
+          {activeTab === tab.id && (
+            <motion.span
+              layoutId="bubble"
+              className="absolute inset-0 z-10 bg-sky-100 mix-blend-difference"
+              style={{ borderRadius: 9999 }}
+              transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+            />
+          )}
+          {tab.label}
+        </button>
+      ))}
+    </div>
+  )
+}
 
 function IndexPopup() {
   const [data, setData] = useState("")
@@ -31,6 +64,7 @@ function IndexPopup() {
           style={{
             padding: 16
           }}>
+          <AnimatedTabs />
           <h2 className="text-gray-900 font-bold text-xl mb-2">
             Let's break it down
           </h2>
@@ -38,7 +72,7 @@ function IndexPopup() {
           <button
             onClick={() => increase()}
             type="button"
-            className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-black bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Simplify Page
           </button>
         </div>
