0c0590a1a8f9d760af4c1a20470b1f3ca6685051
Michael DeMarco <michaelfromyeg@gmail.com>
2024-07-07 16:14:55 -0700

    feat: add prod url to client

--- client/constants.ts
+++ client/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://plasmo.vercel.app"
+    : "https://api.simple-english.michaeldemar.co"
 
 // Send a request to simplify with current url - server responds with short summary
 export const MOCK_SERVER_RESPONSE = `
